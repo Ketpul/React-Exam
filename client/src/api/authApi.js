4e82463d7badefccc78e3564f6f24a36f4003548
Ketpul <emiliqnnik07@abv.bg>
2025-04-02 00:12:03 +0300

    fix

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -41,6 +41,45 @@ export const useLogin = () => {
     return { login, error };
 };
 
+
+export const useRegister = () => {
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    
+    const register = async (email, password, confirmPassword, username) => {
+        if (password !== confirmPassword) {
+            setError('Passwords do not match');
+            return;
+        }
+        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            setError('Invalid email format');
+            return;
+        }
+
+        try {
+            const response = await fetch(`${baseUrl}/register`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password, username }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message || 'Registration failed');
+            }
+            
+            navigate('/login');
+        } catch (error) {
+            setError(error.message);
+            console.error(error);
+        }
+    };
+    
+    return { register, error };
+};
+
 export const useLogout = () => {
     const { setUser } = useContext(UserContext);
 
@@ -64,42 +103,3 @@ export const useLogout = () => {
 
     return { logout };
 };
-
-export const useRegister = () => {
-    const [error, setError] = useState(null);
-    const navigate = useNavigate();
-
-    const register = async (email, password, confirmPassword, username) => {
-        if (password !== confirmPassword) {
-            setError('Passwords do not match');
-            return;
-        }
-
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-            setError('Invalid email format');
-            return;
-        }
-
-        try {
-            const response = await fetch(`${baseUrl}/register`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password, username }),
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message || 'Registration failed');
-            }
-
-            navigate('/login');
-        } catch (error) {
-            setError(error.message);
-            console.error(error);
-        }
-    };
-
-    return { register, error };
-};
-
